0d62256aacf0ffe42b05fb9babb4e38f2693dfef
Devang75 <devangpatil96@gmail.com>
2024-01-08 22:43:12 +0530

    Worked on to add razorpay package.

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,30 @@
 import express from 'express'
+import Razorpay from 'razorpay';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const router = express.Router();
+const razorPayInstance = new Razorpay({
+  key_id: process.env.RAZORPAY_ID_KEY,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
+})
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+// Endpoint to create a Razorpay order
+router.post('/create-order', async (req, res) => {
+  const { amount, currency = 'INR', receipt } = req.body;
+
+  const options = {
+    amount: amount * 100, // Razorpay accepts amount in paise, so multiply by 100
+    currency,
+    receipt,
+  };
+
+  try {
+    const response = await razorPayInstance.orders.create(options);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
